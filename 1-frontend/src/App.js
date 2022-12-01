f5f8de1d6aa8ea172a906ddc1f67d9257fc4d18f
flaviamiranda <flaviagracianom@gmail.com>
2022-12-01 11:06:16 -0300

    added login button, added logout button and user profile info

--- 1-frontend/src/App.js
+++ 1-frontend/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './Components/LoginButton/index.js';
+import LogoutButton from './Components/LogoutButton';
+import Profile from './Components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <LoginButton />
+     <Profile />
+     <LogoutButton />
     </div>
   );
 }

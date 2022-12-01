1d14dee37e494b4b01e2e1095ba6b0447a964535
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-12-01 13:48:39 +0000

    auth0 installed on frontend

--- 1-frontend/src/index.js
+++ 1-frontend/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain="dev-h5z6gl6fimqclhvc.uk.auth0.com"
+    clientId="PbhZvGNcX0rqE6ih6eHrVLMueUH20BLD"
+    redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
